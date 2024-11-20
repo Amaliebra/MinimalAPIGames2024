8fa47825d58426fe973666ca5fac878ddf99796c
Amaliebra <jba082024@gmail.com>
2024-11-20 09:36:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/game", () =>{
+
+})
+
+app.Run();
+
+class Games
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Publisher { get; set; }
+    public double UserMetacriticScore { get; set; }
+}
